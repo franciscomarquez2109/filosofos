d8421dbb24b9c991f8f6f42546a7b26d10271222
Marqueting <marquetingca@gmail.com>
2019-05-26 12:30:18 -0400

    se agregaron nuevos estados de los filosofos

--- filosofos_beta.py
+++ filosofos_beta.py
@@ -3,46 +3,52 @@ import time
 import random
 import threading
 
-N = 5
-TIEMPO_TOTAL = 3
+FILOSOFOS = 5
+VUELTAS = 1
 
 class filosofo(threading.Thread):
     semaforo = threading.Lock() #SEMAFORO BINARIO ASEGURA LA EXCLUSION MUTUA
     estado = [] #PARA CONOCER EL ESTADO DE CADA FILOSOFO
     tenedores = [] #ARRAY DE SEMAFOROS PARA SINCRONIZAR ENTRE FILOSOFOS, MUESTRA QUIEN ESTA EN COLA DEL TENEDOR
-    count=0
+    count= 0
 
     def __init__(self):
         super().__init__()      #HERENCIA
-        self.id=filosofo.count #DESIGNA EL ID AL FILOSOFO
+        self.id = filosofo.count #DESIGNA EL ID AL FILOSOFO
         filosofo.count+=1 #AGREGA UNO A LA CANT DE FILOSOFOS
         filosofo.estado.append('PENSANDO') #EL FILOSOFO ENTRA A LA MESA EN ESTADO PENSANDO
         filosofo.tenedores.append(threading.Semaphore(0)) #AGREGA EL SEMAFORO DE SU TENEDOR( TENEDOR A LA IZQUIERDA)
-        print("FILOSOFO {0} - PENSANDO".format(self.id))
-
-    def __del__(self):
-        print("FILOSOFO {0} - Se para de la mesa".format(self.id))  #NECESARIO PARA SABER CUANDO TERMINA EL THREAD
+        print(" FILOSOFO {0} - SE SIENTA EN LA MESA".format(self.id))
 
     def pensar(self):
-        time.sleep(random.randint(0,5)) #CADA FILOSOFO SE TOMA DISTINTO TIEMPO PARA PENSAR, ALEATORIO
+        time.sleep(random.randint(0,10)) #CADA FILOSOFO SE TOMA DISTINTO TIEMPO PARA PENSAR, ALEATORIO
+        print(" FILOSOFO {} PENSANDO".format(self.id))
 
     def derecha(self,i):
-        return (i-1)%N #BUSCAMOS EL INDICE DE LA DERECHA
+        return (i-1)%FILOSOFOS #BUSCAMOS EL INDICE DE LA DERECHA
 
     def izquierda(self,i):
-        return(i+1)%N #BUSCAMOS EL INDICE DE LA IZQUIERDA
-
-    def verificar(self,i):
-        if filosofo.estado[i] == 'HAMBRIENTO' and filosofo.estado[self.izquierda(i)] != 'COMIENDO' and filosofo.estado[self.derecha(i)] != 'COMIENDO':
-            filosofo.estado[i]='COMIENDO'
-            filosofo.tenedores[i].release()  #SI SUS VECINOS NO ESTAN COMIENDO AUMENTA EL SEMAFORO DEL TENEDOR Y CAMBIA SU ESTADO A COMIENDO
+        return(i+1)%FILOSOFOS #BUSCAMOS EL INDICE DE LA IZQUIERDA
 
     def tomar(self):
         filosofo.semaforo.acquire() #SEÑALA QUE TOMARA LOS TENEDORES (EXCLUSION MUTUA)
         filosofo.estado[self.id] = 'HAMBRIENTO'
+        print(" FILOSOFO {} HAMBRIENTO".format(self.id))
         self.verificar(self.id) #VERIFICA SUS VECINOS, SI NO PUEDE COMER NO SE BLOQUEARA EN EL SIGUIENTE ACQUIRE
         filosofo.semaforo.release() #SEÑALA QUE YA DEJO DE INTENTAR TOMAR LOS TENEDORES (CAMBIAR EL ARRAY ESTADO)
         filosofo.tenedores[self.id].acquire() #SOLO SI PODIA TOMARLOS SE BLOQUEARA CON ESTADO COMIENDO
+
+    def verificar(self,i):
+        if filosofo.estado[i] == 'HAMBRIENTO' and filosofo.estado[self.izquierda(i)] != 'COMIENDO' and filosofo.estado[self.derecha(i)] != 'COMIENDO':
+            filosofo.estado[i]='COMIENDO' #CAMBIA SU ESTADO A COMIENDO
+            filosofo.tenedores[i].release()  #SI SUS VECINOS NO ESTAN COMIENDO AUMENTA EL SEMAFORO DEL TENEDOR
+
+
+
+    def comer(self):
+        print(" FILOSOFO {} COMIENDO".format(self.id))
+        time.sleep(2) #TIEMPO ARBITRARIO PARA COMER
+        print(" FILOSOFO {} TERMINO DE COMER ".format(self.id))
 
     def soltar(self):
         filosofo.semaforo.acquire() #SEÑALA QUE SOLTARA LOS TENEDORES
@@ -51,13 +57,12 @@ class filosofo(threading.Thread):
         self.verificar(self.derecha(self.id))
         filosofo.semaforo.release() #YA TERMINO DE MANIPULAR TENEDORES
 
-    def comer(self):
-        print("FILOSOFO {} COMIENDO".format(self.id))
-        time.sleep(2) #TIEMPO ARBITRARIO PARA COMER
-        print("FILOSOFO {} TERMINO DE COMER".format(self.id))
+
+    def __del__(self):
+        print(" FILOSOFO {0} - Se para de la mesa".format(self.id))  #NECESARIO PARA SABER CUANDO TERMINA EL THREAD
 
     def run(self):
-        for i in range(TIEMPO_TOTAL):
+        for i in range(VUELTAS):
             self.pensar() #EL FILOSOFO PIENSA
             self.tomar() #AGARRA LOS TENEDORES CORRESPONDIENTES
             self.comer() #COME
@@ -65,7 +70,7 @@ class filosofo(threading.Thread):
 
 def main():
     lista=[]
-    for i in range(N):
+    for i in range(FILOSOFOS):
         lista.append(filosofo()) #AGREGA UN FILOSOFO A LA LISTA
 
     for f in lista:
